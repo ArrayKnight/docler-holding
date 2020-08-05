99e329535af78e0a808de7b7ed8b33a86fe4c6d6
Ray Knight <array.knight+github@gmail.com>
2020-08-05 12:01:18 -0700

    feat: 🎸 dev toCamelCase logic

--- src/toCamelCase.test.ts
+++ src/toCamelCase.test.ts
@@ -3,11 +3,22 @@ import { toCamelCase } from './toCamelCase'
 describe('toCamelCase', () => {
     it('should convert string to camel case', () => {
         const pairs = [
+            ['', ''],
+            [' ', ''],
+            [' foo', 'foo'],
+            [' foo ', 'foo'],
             ['foo', 'foo'],
             ['Foo', 'foo'],
             ['FooBar', 'fooBar'],
             ['Foo-Bar', 'fooBar'],
             ['foo bar', 'fooBar'],
+            [' fooBar', 'fooBar'],
+            [' foo bar', 'fooBar'],
+            [' foo-bar Baz', 'fooBarBaz'],
+            ['-foo-bar-baz', 'fooBarBaz'],
+            ['FOOBAR', 'foobar'],
+            ['fooID', 'fooId'],
+            ['fooBAR', 'fooBar'],
         ]
 
         pairs.forEach(([input, output]) => {
--- src/toCamelCase.ts
+++ src/toCamelCase.ts
@@ -1,3 +1,18 @@
 export function toCamelCase(input: string): string {
-    return input
+    return input.split(/[^a-z]/i).reduce((output, word) => {
+        if (!word) {
+            return output
+        }
+
+        word = word.replace(
+            /([A-Z])([A-Z]+)/g,
+            (_: string, p1: string, p2: string) => `${p1}${p2.toLowerCase()}`,
+        )
+
+        if (!output) {
+            return `${word[0].toLowerCase()}${word.slice(1)}`
+        }
+
+        return `${output}${word[0].toUpperCase()}${word.slice(1)}`
+    }, '')
 }
